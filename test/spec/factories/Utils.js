2b53e8d73c83010b1def1025e9fa7e742f06f2d2
Max Barrett <maxjbarrett@gmail.com>
2015-05-27 22:51:02 +1000

    add tests

--- test/spec/factories/Utils.js
+++ test/spec/factories/Utils.js
@@ -1,16 +1,60 @@
 describe('Utils', function() {
 
-    var theFactory; 
+    var theFactory,
+    	days,
+    	hours,
+        mapsData;
 
     beforeEach(module('forecastApp'));
 
     beforeEach(inject(function(Utils) {
             theFactory = Utils;
+
+            days = [{
+                'time': 1432648800,
+            }, {
+                'time': 1432735200,
+            }, {
+                'time': 1432821600,
+            }],
+            hours = [{
+                'time': 1432677600,
+            }, {
+                'time': 1432681200,
+            }, {
+                'time': 1432684800,
+            }];
+
+            mapsData = {
+                "data": {
+                    "results": [{
+                        "geometry": {
+                            "location": {
+                                "lat": 25.2048493,
+                                "lng": 55.2707828
+                            }
+                        }
+                    }]
+                }
+            }
+
         })
     );
 
-    it('should slice the right number from an array', function() {
-        expect(theFactory.slice([1,2,3,4,5,6,7,8,9], 5)).toEqual([1,2,3,4,5]);
+    it('should convert object of timestamps into day names', function() {
+        expect(theFactory.nameDays(days)).toEqual(['Wednesday', 'Thursday', 'Friday']);
+    });
+
+	it('should convert object of timestamps into hours', function() {
+		expect(theFactory.time(hours)).toEqual(['08', '09', '10']);
+	});
+
+	it('should get data for required day', function() {
+		expect(theFactory.pickDay(days, 'thursday')).toEqual({time:1432735200});
+	});
+
+    it('should get lat/long coordinates from json', function() {
+        expect(theFactory.getLatLong(mapsData)).toEqual('25.2048493,55.2707828');
     });
 
 });
--- test/spec/factories/dateFactory.js
+++ test/spec/factories/dateFactory.js
@@ -9,9 +9,30 @@ describe('dateFactory', function() {
         })
     );
 
-    it('should show the correct date from a unix timestamp', function() {
+    it('should format the date from a unix timestamp', function() {
         expect(theFactory.formatDate(1432645880)).toEqual('Tuesday 26 May 2015');
     });
+
+    it('should get the correct hour from a date object', function() {
+        expect(theFactory.getHour(new Date('Wed May 27 2015 22:00:00 GMT+1000 (AEST)'))).toEqual('22');
+    });
+
+    it('should extract the day from a date object', function() {
+        expect(theFactory.getDay(new Date('Wed May 27 2015 22:00:00 GMT+1000 (AEST)'))).toEqual(3);
+    });
+
+    it('should show day name from index', function() {
+        expect(theFactory.getDayName(3)).toEqual('Wednesday');
+    });
+
+    it('should create a new date object from a unix timestamp', function() {
+        expect(theFactory.getDate(1432670400)).toEqual(new Date('Wed May 27 2015 06:00:00 GMT+1000 (AEST)'));
+    });
+
+    it('should show the correct date from a unix timestamp', function() {
+        expect(theFactory.getMonth(5)).toEqual('June');
+    });
+
 
 });
 
