ca68259e50f282feeb2714ee39c5d48f32209395
Max Barrett <maxjbarrett@gmail.com>
2015-05-28 23:25:25 +1000

    change to https

--- client/scripts/factories/GeocodingAPI.js
+++ client/scripts/factories/GeocodingAPI.js
@@ -5,7 +5,7 @@ angular.module('forecastApp')
 	    return {
 	        get: function(location){
 	        	// get the lat/long coordinates of a location from google api
-	        	return $http.get('http://maps.google.com/maps/api/geocode/json?address=' + location + '&sensor=false').then(function(result) {
+	        	return $http.get('https://maps.google.com/maps/api/geocode/json?address=' + location + '&sensor=false').then(function(result) {
           			return result.data;
         		});
 	        }
