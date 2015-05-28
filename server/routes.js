ae707b20b12fdd1c4acd4de1c0c09cf0a51432ea
Max Barrett <maxjbarrett@gmail.com>
2015-05-28 23:34:48 +1000

    server routing

--- server/routes.js
+++ server/routes.js
@@ -4,13 +4,18 @@ var errors = require('./errors');
 
 module.exports = function(app) {
 
-    // All undefined asset or api routes should return a 404
-    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-        .get(errors[404]);
+    // // All undefined asset or api routes should return a 404
+    // app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+    //     .get(errors[404]);
 
-    // All other routes should redirect to the index.html
-    app.route('/*')
-        .get(function(req, res) {
-        	res.sendFile('index.html', { root: __dirname + '/../client/' });
-        });
+    // // All other routes should redirect to the index.html
+    // app.route('/*')
+    //     .get(function(req, res) {
+    //     	res.sendFile('index.html', { root: __dirname + '/../client/' });
+    //     });
+
+
+	app.get('/*', function(req, res){
+		res.sendFile('index.html', { root: __dirname + '/../client/' });
+	});
 };
